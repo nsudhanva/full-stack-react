2b219c013a61eab0af6016f303bfbae7785a81f8
Sudhanva Narayana <nsudhanva@gmail.com>
2018-09-09 16:30:03 +0530

    Mongoose setup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/client_id.json');
+
 require('./services/passport')
+
+mongoose.connect(keys.web.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
